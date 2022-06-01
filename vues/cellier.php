3ce0125f319f44c2cdfb086e27531909f689eb48
e8506278 <e8506278@cmaisonneuve.qc.ca>
2022-06-01 13:59:16 -0400

    Ajout de PW2.sql et modification de 3 fichiers. Chercher 'CR - DEB - MODIF' pour trouver les modif.

--- vues/cellier.php
+++ vues/cellier.php
@@ -1,34 +1,33 @@
 <div class="cellier">
-<?php
-foreach ($data as $cle => $bouteille) {
- 
+    <?php
+    foreach ($data as $cle => $bouteille) {
+
     ?>
-    <div class="bouteille" data-quantite="">
-        <div class="img">
-            
-            <img src="https:<?php echo $bouteille['image'] ?>">
+        <!-- * CR - DEB - MODIF * -->
+        <div class="bouteille" data-quantite="">
+            <div class="img">
+                <img src="<?php echo $bouteille['image'] ?>">
+            </div>
+            <div class="description">
+                <p class="nom">Nom : <?php echo $bouteille['nom'] ?></p>
+                <p class="quantite">Quantité : <?php echo $bouteille['quantite'] ?></p>
+                <p class="pays">Pays : <?php echo $bouteille['nom_pays'] ?></p>
+                <p class="type">Catégorie : <?php echo $bouteille['nom_categorie'] ?></p>
+                <p class="millesime">Millesime : <?php echo $bouteille['millesime'] ?></p>
+                <p><a href="<?php echo $bouteille['url_saq'] ?>">Voir SAQ</a></p>
+            </div>
+            <div class="options" data-id="<?php echo $bouteille['id_bouteille_cellier'] ?>">
+                <button>Modifier</button>
+                <button class='btnAjouter'>Ajouter</button>
+                <button class='btnBoire'>Boire</button>
+
+            </div>
         </div>
-        <div class="description">
-            <p class="nom">Nom : <?php echo $bouteille['nom'] ?></p>
-            <p class="quantite">Quantité : <?php echo $bouteille['quantite'] ?></p>
-            <p class="pays">Pays : <?php echo $bouteille['pays'] ?></p>
-            <p class="type">Type : <?php echo $bouteille['type'] ?></p>
-            <p class="millesime">Millesime : <?php echo $bouteille['millesime'] ?></p>
-            <p><a href="<?php echo $bouteille['url_saq'] ?>">Voir SAQ</a></p>
-        </div>
-        <div class="options" data-id="<?php echo $bouteille['id_bouteille_cellier'] ?>">
-            <button>Modifier</button>
-            <button class='btnAjouter'>Ajouter</button>
-            <button class='btnBoire'>Boire</button>
-            
-        </div>
-    </div>
-<?php
+        <!-- * CR - FIN - MODIF -->
+    <?php
 
 
-}
+    }
 
-?>	
+    ?>
 </div>
-
-
