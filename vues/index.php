112b111bf65aeb0ed0b243ac06bd0122366c2269
Genevieve <e0059486@cmaisonneuve.qc.ca>
2022-06-03 18:23:28 -0400

    rectification css

---
+++ vues/index.php
@@ -0,0 +1,47 @@
+
+<section class="section-wrapper banderole">
+			<div class="grille grille--3 ">
+				<div class="banderole__contenant">
+					<div>Celliers</div>
+					<div>2</div>
+				</div>
+				<div class="banderole__contenant">
+					<div>Bouteilles</div>
+					<div>50</div>
+				</div>
+				<div class="banderole__contenant">
+					<div>À boire</div>
+					<div>2</div>
+				</div>
+			
+			</div>
+		</section>
+		<section  class="section-wrapper vignette">
+		
+
+			<h4 class="vignette__entete">Gérer mes collections</h4>
+			<br>
+			<!--Espace 1-->
+			<div class="grille grille--4">
+				<figure class="vignette__wrapper">
+					<img class="vignette__img" src="./assets/img/cellier2.jpg" alt="cellier">
+					<figcaption class="vignette__titre">Mes celliers</figcaption>
+				</figure>
+			<!--Espace 2-->
+				<figure class="vignette__wrapper">
+					<img class="vignette__img" src="./assets/img/bouteilles1.jpg" alt="bouteille">
+					<figcaption class="vignette__titre">Mes bouteilles</figcaption>
+				</figure>
+			<!--Espace 3-->
+				<figure class="vignette__wrapper">
+					<img class="vignette__img" src="./assets/img/bouchonblanc.jpg" alt="bouteilles">
+					<figcaption class="vignette__titre">Ajouter une bouteille</figcaption>
+				</figure>
+			<!--Espace 4 -->
+				<figure class="vignette__wrapper">
+					<img class="vignette__img" src="./assets/img/etagere.jpg" alt="bouteilles">
+					<figcaption class="vignette__titre">Ma liste d'achat</figcaption>
+				</figure>
+			</div>
+			
+		</section>
