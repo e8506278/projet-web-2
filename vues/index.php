6212aae06f8197266f6dd141690ad56e8545091e
Genevieve <e0059486@cmaisonneuve.qc.ca>
2022-06-03 21:27:56 -0400

    ajout boite-double et texte accueil

--- vues/index.php
+++ vues/index.php
@@ -1,4 +1,61 @@
+<!--Page d'accueil non connecté-->
 
+<section class="section-wrapper texte-photo">
+	
+<h1 class="texte-photo__titre">La meilleure application de gestion de cellier disponible sur le marché</h1>
+	<article class="texte-photo__contenu">
+		
+
+		<p class="texte-photo__texte">Vino est l'application la plus complète sur le marché vous permettant de gerer jusqu'à <span class="text-photo__texte--gras">10 celliers différents</span>. 
+			Simple à utiliser, l'application vous tiendra à jour des vins <span class="text-photo__texte--gras">prêts à boire</span>, vous permet d'y tenir une <span class="text-photo__texte--gras">liste d'achat</span> et d'avoir des <span class="text-photo__texte--gras">statistiques</span> sur les entrées et sorties des vins de votre cellier.
+			
+		</p>
+		<div class="texte-photo__img">
+			<img  src="./assets/img/cellier.jpg" alt="degustation2">
+		</div>
+
+		
+	</article>
+
+</section>
+<section class="section-wrapper">
+	<div class="boite-double">
+		<!--LOGIN-->
+		<div class="boite-double__contenant-form">
+			<h3>Connectez-vous</h3>
+			<form class="formulaire">
+				<div class="formulaire__champs boite-double__champs">
+					<input placeholder="Nom d'usager">
+				</div>
+				<div class="formulaire__champs boite-double__champs">
+					<input placeholder="Mot de passe">
+				</div>
+				<div class="formulaire__champs">
+					<button class="bouton-secondaire ">Se connecter</button>
+				</div>	
+			</form>
+		</div>
+		<!--S'INSCRIRE-->
+		<div class="boite-double__contenant-texte">
+			<h3>Vous n'avez-pas de compte?</h3>
+			<div>
+				<p class="boite-double__texte"> Commencez dès aujourd'hui à gérer vos celliers!</p>
+				<ul>
+					<li><span class="boite-double__texte--couleur">&#10148;</span> Possibilité de plusieurs celliers.</li>
+					<li><span class="boite-double__texte--couleur">&#10148;</span> Recherchez des bouteilles et ajoutez les à votre cellier.</li>
+					<li><span class="boite-double__texte--couleur">&#10148;</span> Faites votre liste d'achat, partagez sur les réseaux sociaux et bien plus!</li>
+				</ul>
+			</div>
+			<div class="boite-double__texte-bouton">
+				<button class="bouton-secondaire ">Je m'inscris!</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<!--
 <section class="section-wrapper banderole">
 			<div class="grille grille--3 ">
 				<div class="banderole__contenant">
@@ -21,23 +78,23 @@
 
 			<h4 class="vignette__entete">Gérer mes collections</h4>
 			<br>
-			<!--Espace 1-->
+
 			<div class="grille grille--4">
 				<figure class="vignette__wrapper">
 					<img class="vignette__img" src="./assets/img/cellier2.jpg" alt="cellier">
 					<figcaption class="vignette__titre">Mes celliers</figcaption>
 				</figure>
-			<!--Espace 2-->
+
 				<figure class="vignette__wrapper">
 					<img class="vignette__img" src="./assets/img/bouteilles1.jpg" alt="bouteille">
 					<figcaption class="vignette__titre">Mes bouteilles</figcaption>
 				</figure>
-			<!--Espace 3-->
+	
 				<figure class="vignette__wrapper">
 					<img class="vignette__img" src="./assets/img/bouchonblanc.jpg" alt="bouteilles">
 					<figcaption class="vignette__titre">Ajouter une bouteille</figcaption>
 				</figure>
-			<!--Espace 4 -->
+
 				<figure class="vignette__wrapper">
 					<img class="vignette__img" src="./assets/img/etagere.jpg" alt="bouteilles">
 					<figcaption class="vignette__titre">Ma liste d'achat</figcaption>
@@ -45,3 +102,5 @@
 			</div>
 			
 		</section>
+-->
+
