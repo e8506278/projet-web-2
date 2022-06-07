aec89bd902decb1025cfe35bfd477829c6105282
Frahli10 <86541853+Frahli10@users.noreply.github.com>
2022-06-07 14:44:29 +0100

    Update details.php

--- vues/details.php
+++ vues/details.php
@@ -10,6 +10,7 @@
 
 	<article class="texte-photo__contenu">
 
+
         <div class="product-photo">
             <img  src="<?php echo $product['image']?>" alt="degustation2">
         </div>
@@ -19,106 +20,21 @@
             <h3 class="under-texte-photo__texte">
                 <?php echo $product['description']?>
             </h3>
-            <div class="info">
-                <p class="rating">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                </p>
-                <h2 class="price">
-                    <?php echo $product['prix_saq']?> $
-                </h2>
-            </div>
-            <hr class="separator" />
+            <br><br>
+            
+<div class="tabset">
+    <!-- Tab 1 -->
+    <input type="radio" name="tabset" id="tab1" aria-controls="marzen" checked>
+    <label for="tab1">Infos</label>
+    <!-- Tab 2 -->
+    <input type="radio" name="tabset" id="tab2" aria-controls="rauchbier">
+    <label for="tab2">Details</label>
+    
+    
+    <div class="tab-panels">
+      <section id="marzen" class="tab-panel">
+        <div class="info-details">
             <div class="row">
-                <p class="quantity col-6 normal-text">
-                    <i class="fa fa-check" aria-hidden="true"></i>
-                    <b>103</b> disponible(s) en ligne **HC**
-                </p>
-            </div>
-            <div class="row">
-                <div class="fieldset fieldset-qty-tocart margin-column-10 col">
-                    <div class="field qty">
-                        <div class="control">
-                            <input type="number" name="qty" id="qty" value="1" class="input-text ">
-                        </div>
-                    </div>
-                    <div class="actions">
-                        <button type="submit" title="Ajouter à la liste d'achat"
-                                class="bouton-secondaire" id="product-addtocart-button">
-                            <span>Ajouter à la liste d'achat</span>
-                        </button>
-                    </div>
-                </div>
-
-                <div class="fieldset fieldset-qty-tocart margin-column-10 col">
-                    <div class="field qty">
-                        <div class="control">
-                            <input type="number" name="qty" id="qty" value="1" class="input-text ">
-                        </div>
-                    </div>
-                    <div class="actions">
-                        <button type="submit" title="Ajouter à la liste d'achat"
-                                class="bouton-secondaire" id="product-addtocart-button">
-                            <span>Ajouter à mon cellier</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <hr class="separator" />
-            <h4>Choisir un cellier</h4>**HC**
-            <div class="cellier bouton-primaire">
-                <i class="fa fa-check"></i> &nbsp;
-                Cellier 1
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 2
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 3
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 4
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 5
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 6
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 7
-            </div>
-            <div class="cellier bouton-primaire">
-                Cellier 8
-            </div>
-
-            <hr class="separator" />
-            <p class="normal-text action margin-column-10">
-                <i class="fa fa-heart-o" aria-hidden="true"></i>
-                Ajouter au favoris
-            </p>
-            <p class="normal-text action margin-column-10">
-                <i class="fa fa-beer" aria-hidden="true"></i>
-                À essayer
-            </p>
-        </div>
-
-		
-	</article>
-
-</section>
-<section class="section-wrapper">
-	<div class="boite-double">
-		<!--LOGIN-->
-		<div class="boite-double__contenant-form">
-			<h2>Infos détaillées</h2>
-		</div>
-
-		<div class="info-details">
-			<div class="row">
                 <div class="col-6 info-unit">
                     <div class="label">Pays</div>
                     <div class="value"><?php echo $product['nom_pays']?></div>
@@ -178,35 +94,90 @@
                     <div class="value">00012354001947 **HC**</div>
                 </div>
             </div>
-		</div>
-	</div>
-</section>
-<section class="section-wrapper texte-photo">
-    <div class="boite-double">
-        <!--LOGIN-->
-        <div class="boite-double__contenant-form header-with-sub">
-            <h2>Avis</h2>
-            <div class="sub-header">
-                <p class="rating bigger-rating">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                </p> <p class="small-text">| 65 avis</p>
+            <div class="row">
+                <button class="bouton-secondaire w-100 submit-button">
+                   Button 1
+                </button>
             </div>
-            <div class="sub-header">
-                <h2>3,7</h2>&nbsp;&nbsp;sur 5
-            </div>
-            <div class="sub-header">
-                <div class="note">
-                    Les avis que vous soumettez doivent respecter notre politique de modération.
-                    <p class="action">Voir la politique de modération de la SAQ.</p>
+           
+        </div>
+       </section>
+        <section id="rauchbier" class="tab-panel">
+            <div class="info-details">
+                <div class="row">
+                    <div class="col-6 info-unit">
+                        <div class="value">Prix</div>
+                        <div class="value">
+                            <input type='number' style="max-width:70% ;" >  
+                        </div>
+                    </div>
+                    <div class="col-6 info-unit">
+                        <div class="value">Quantite</div>
+                        <div class="value">
+                            <input type='number' style="max-width:70% ;" >  
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 info-unit">
+                        <div class="value">Millesime</div>
+                        <div class="value">
+                            <input type='number' style="max-width:70% ;" >  
+                        </div>
+                    </div>
+                    <div class="col-6 info-unit">
+                        <div class="value">Garde jusqu'a</div>
+                        <div class="value">
+                            <input type='date' style="max-width:70% ;" >  
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 info-unit">
+                        <div class="value">Date d'achat</div>
+                        <div class="value">
+                            <input type='date' style="max-width:70% ;" >  
+                        </div>
+                    </div>
+                    <div class="col-6 info-unit">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 info-unit">
+                        <button class="btn bouton-secondaire">
+                            Button 2
+                        </button>
+                    </div>
+                    <div class="col-6 info-unit">
+                        <button class="btn bouton-secondaire">
+                            Button 3
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+    </div>
+    
+  </div>
+  
+            
+		</div>
 
-        <div class="info-details">
+
+            
+
+		
+	</article>
+
+</section>
+
+
+
+<section class="section-wrapper texte-photo">
+        <!--LOGIN-->
+       
+
+        <div style="text-align:center ;">
             <div class='comments'>
 <!--                this is for a nnew comment-->
                 <div class="comment" >
@@ -239,129 +210,18 @@
                             </div>
                         </div>
                         <div class="comment-text new-comment">
-                            <input placeholder='Ajouter un nouveau commentaire'
-                                   type="text"
-                                   class="input formulaire__champs boite-double__champs" />
+                            <textarea 
+                                   class="input formulaire__champs boite-double__champs" ></textarea>
                             <button class="btn bouton-secondaire">Soumettre</button>
                         </div>
                     </div>
                 </div>
-<!--                we should do listing here-->
-                <div class="comment" >
-                    <div class="comment-body">
-                        <div class="comment-header">
-                            <p class="rating">
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
-                            </p>
-                        </div>
-                        <div class="comment-header">
-                            <b>Miam</b>
-                            <span class="light-text">Par Sabrina E.le 22 janvier 2022</span>
-                        </div>
-                        <div class="comment-text">
-                            Le 16% fait son charme (ça soule), mais il manque de carrure.
-                        </div>
-                    </div>
-                </div>
-                <div class="comment" >
-                    <div class="comment-body">
-                        <div class="comment-header">
-                            <p class="rating">
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
-                            </p>
-                        </div>
-                        <div class="comment-header">
-                            <b>Très déçu</b>
-                            <span class="light-text">Par Dominic G.le 14 avril 2022</span>
-                        </div>
-                        <div class="comment-text">
-                            Trop sucré, le rouge est bien meilleur
-                        </div>
-                    </div>
-                </div>
-                <div class="comment" >
-                    <div class="comment-body">
-                        <div class="comment-header">
-                            <p class="rating">
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
-                            </p>
-                        </div>
-                        <div class="comment-header">
-                            <b>Bof</b>
-                            <span class="light-text">Par Frederic F.le 4 novembre 2021</span>
-                        </div>
-                        <div class="comment-text">
-                            Ben ordinaire beaucoup trop sucré !!!
-                        </div>
-                    </div>
-                </div>
-                <div class="comment" >
-                    <div class="comment-body">
-                        <div class="comment-header">
-                            <p class="rating">
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
-                            </p>
-                        </div>
-                        <div class="comment-header">
-                            <b>Très bon vin</b>
-                            <span class="light-text">Par Lyse B.le 20 octobre 2021</span>
-                        </div>
-                        <div class="comment-text">
-                            J'ai adoré ce vin très goûteux doux en bouche.
-                            Malheureusement non disponible dans toutes les succursale.
-                        </div>
-                    </div>
-                </div>
-                <div class="comment" >
-                    <div class="comment-body">
-                        <div class="comment-header">
-                            <p class="rating">
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
-                            </p>
-                        </div>
-                        <div class="comment-header">
-                            <b>19 Crimes Hard Chard</b>
-                            <span class="light-text">Par Lucie Hudon A.le 19 octobre 2021</span>
-                        </div>
-                        <div class="comment-text">
-                            Je le voyais souvent dans le vin rouge et je l'offrais en cadeau et puis je l'ai trouvé dans le vin blanc et je l'adore. Je suis plus vin blanc et je le recommande.
-                        </div>
-                    </div>
-                </div>
-                <div class="pagination">
-<!--                    <i class="fa fa-arrow-left" aria-hidden="true"></i>-->
-                    <span class="current_page item">1</span>
-                    <span class="item">2</span>
-                    <span class="item">3</span>
-                    &nbsp;&nbsp;
-                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-                </div>
-                <!--   end loop comments-->
             </div>
 
-        </div>
     </div>
 </section>
+
+
 <section class="section-wrapper">
     <div class="boite-double flex-column">
         <div class="boite-double__contenant-form">
