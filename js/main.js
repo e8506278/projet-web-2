9d33acc408aea74e071fa71b8d775e3639de753c
Genevieve <e0059486@cmaisonneuve.qc.ca>
2022-06-15 17:02:01 -0400

    ajout du modal de suppression d'un cellier

--- js/main.js
+++ js/main.js
@@ -13,90 +13,6 @@ const BaseURL = document.baseURI;
 
 window.addEventListener('load', function () {
 
-// RÉDUIRE LA QTÉ D'UNE BOUTEILLE DANS UN CELLIER
-
-    document.querySelectorAll(".btnBoire").forEach(function (element) {
-       
-       // Au click du bouton diminué (-)
-        element.addEventListener("click", function (e) {
-            e.preventDefault();
-            // id_bouteille
-            let id =element.parentElement.dataset.jsId;
-
-            
-            let requete = new Request(BaseURL + "?requete=reduireQteBouteille", { method: 'POST', body: '{"id": ' + id + '}' });
-
-            // Requête fetch
-            fetch(requete)
-                .then(response => {
-                    if (response.status === 200) {
-                        return response;
-                    } else {
-                        throw new Error('Erreur');
-                    }
-                })
-                .then(response => {
-                    // Récuperation des cartes bouteille
-                    let carteBouteille = document.querySelectorAll('[data-js-bouteille]')
-
-                    // Récupération du nombre de bouteille dans le <span></span>
-                    nombreBouteille = element.previousElementSibling.textContent
-
-                    // Pour chaque carte bouteille
-                    for (let i = 0, l = carteBouteille.length; i < l; i++){
-                        // Si le dataset de l'id_bouteille correspond au id_bouteille du  click
-                        if(carteBouteille[i].dataset.jsBouteille == id){
-                            // Si le nombre de bouteille est égal à 1
-                            if(nombreBouteille == 1){
-                                // Suppression à l'écran de la carte bouteille car le nombre de bouteille tombe à zéro
-                                carteBouteille[i].remove();
-                            }
-                        }
-                    }
-                    // Si le nombre de bouteille est supérieur ou égal à 1
-                    if(nombreBouteille >= 1){
-                        // Insertion dans le DOM de la nouvelle quantité
-                        element.previousElementSibling.innerHTML = `<span data-js-quantite>${nombreBouteille-1}</span>`; 
-                    }
-                }).catch(error => {
-                    console.error(error);
-                });  
-        })
-    })
-
-// AJOUTER LA QTÉ D'UNE BOUTEILLE DANS UN CELLIER
-    document.querySelectorAll(".btnAjouter").forEach(function (element) {
-        
-        // Au click du bouton ajouter (+)
-        element.addEventListener("click", function (e) {
-            e.preventDefault();
-            // id_bouteille
-            let id =element.parentElement.dataset.jsId;
-
-            let requete = new Request(BaseURL + "?requete=ajouterQteBouteille", { method: 'POST', body: '{"id": ' + id + '}' });
-
-            // Requête fetch
-            fetch(requete)
-                .then(response => {
-                    if (response.status === 200) {
-                        return response;
-                    } else {
-                        throw new Error('Erreur');
-                    }
-                })
-                .then(response => {
-
-                    // Récupération de l'id_bouteille dans le <span></span>
-                    nombreBouteille = parseInt(element.nextElementSibling.textContent);
-                     // Insertion dans le DOM de la nouvelle quantité
-                    element.nextElementSibling.innerHTML = `<span data-js-quantite>${(nombreBouteille)+1}</span>`
-        
-                }).catch(error => {
-                    console.error(error);
-                });
-        })
-
-    });
 
 
     let inputNomBouteille = document.querySelector("[name='nom_bouteille']");
@@ -190,11 +106,100 @@ window.addEventListener('load', function () {
     }
 
 
-// GESTION D'UN CELLIER
+/********************************************************************************************************** */
+/*                       CELLIER - GESTION QUANTITÉ D'UNE BOUTEILLE 
+                 À PARTIR DE LA PAGE CELLIERS, AJOUTER OU BOIRE UNE BOUTEILLE       *GM                     */
+/********************************************************************************************************** */
+
+// RÉDUIRE LA QTÉ D'UNE BOUTEILLE DANS UN CELLIER 
+
+document.querySelectorAll(".btnBoire").forEach(function (element) {
+       
+    // Au click du bouton diminué (-)
+     element.addEventListener("click", function (e) {
+         e.preventDefault();
+         // id_bouteille
+         let id =element.parentElement.dataset.jsId;
+         
+         let requete = new Request(BaseURL + "?requete=reduireQteBouteille", { method: 'POST', body: '{"id": ' + id + '}' });
+
+         // Requête fetch
+         fetch(requete)
+             .then(response => {
+                 if (response.status === 200) {
+                     return response;
+                 } else {
+                     throw new Error('Erreur');
+                 }
+             })
+             .then(response => {
+                 // Récuperation des cartes bouteille
+                 let carteBouteille = document.querySelectorAll('[data-js-bouteille]')
+
+                 // Récupération du nombre de bouteille dans le <span></span>
+                 nombreBouteille = element.previousElementSibling.textContent
+
+                 // Pour chaque carte bouteille
+                 for (let i = 0, l = carteBouteille.length; i < l; i++){
+                     // Si le dataset de l'id_bouteille correspond au id_bouteille du  click
+                     if(carteBouteille[i].dataset.jsBouteille == id){
+                         // Si le nombre de bouteille est égal à 1
+                         if(nombreBouteille == 1){
+                             // Suppression à l'écran de la carte bouteille car le nombre de bouteille tombe à zéro
+                             carteBouteille[i].remove();
+                         }
+                     }
+                 }
+                 // Si le nombre de bouteille est supérieur ou égal à 1
+                 if(nombreBouteille >= 1){
+                     // Insertion dans le DOM de la nouvelle quantité
+                     element.previousElementSibling.innerHTML = `<span data-js-quantite>${nombreBouteille-1}</span>`; 
+                 }
+             }).catch(error => {
+                 console.error(error);
+             });  
+     })
+ })
+
+// AJOUTER LA QTÉ D'UNE BOUTEILLE DANS UN CELLIER 
+ document.querySelectorAll(".btnAjouter").forEach(function (element) {
+     
+     // Au click du bouton ajouter (+)
+     element.addEventListener("click", function (e) {
+         e.preventDefault();
+         // id_bouteille
+         let id =element.parentElement.dataset.jsId;
+
+         let requete = new Request(BaseURL + "?requete=ajouterQteBouteille", { method: 'POST', body: '{"id": ' + id + '}' });
+
+         // Requête fetch
+         fetch(requete)
+             .then(response => {
+                 if (response.status === 200) {
+                     return response;
+                 } else {
+                     throw new Error('Erreur');
+                 }
+             })
+             .then(response => {
+
+                 // Récupération de l'id_bouteille dans le <span></span>
+                 nombreBouteille = parseInt(element.nextElementSibling.textContent);
+                  // Insertion dans le DOM de la nouvelle quantité
+                 element.nextElementSibling.innerHTML = `<span data-js-quantite>${(nombreBouteille)+1}</span>`
+     
+             }).catch(error => {
+                 console.error(error);
+             });
+     })
+
+ });
+/********************************************************************************************************** */
+/*              GESTION DES CELLIERS - AJOUTER, SUPPRIMER, MODIFIER        *GM                              */
+/********************************************************************************************************** */
 
     let elBtnAjouterCellier =  document.querySelector('[data-js-boutonAjouterCellier]'),
         elBtnModifierInfosCellier = document.querySelector('[data-js-modifierInfosCellier]'),
-        
         usager =  document.querySelector("[data-js-usager]"),    
         erreurnom =  document.querySelector("[data-js-erreurNom]"),
         erreurradio =  document.querySelector("[data-js-erreurRadio]"),
@@ -205,9 +210,10 @@ window.addEventListener('load', function () {
         estValide = true,
         id_cellier = ""
         
-    // Ajouter cellier    
+
+/*---------Ajouter cellier ------------------*/  
+
     if(elBtnAjouterCellier){
-       
         elBtnAjouterCellier.addEventListener('click', (e) => {
             e.preventDefault();
                 
@@ -234,9 +240,11 @@ window.addEventListener('load', function () {
     }
     
 
-    // Modification d'un cellier
-        // 1- Afficher les champs à modifier
-     document.querySelectorAll('[data-js-carte]').forEach(function(carte){
+
+/*---------Modification d'un cellier------------------*/
+
+    // 1- Afficher les champs à modifier
+    document.querySelectorAll('[data-js-carte]').forEach(function(carte){
         // Récupération du cellier à modifier
         carte.querySelectorAll('[data-js-modifiercellier]').forEach(function (btnModalModifier) {
            // Au click
@@ -285,8 +293,7 @@ window.addEventListener('load', function () {
                     "description_cellier": description_cellier.value
                     
                 };  
-                
-               
+                  
                 // Requête fetch
                 let requete = new Request(BaseURL + "?requete=modifierCellier", { method: 'POST', body: JSON.stringify(cellier) });
                 fetchCellier(requete)         
@@ -296,7 +303,11 @@ window.addEventListener('load', function () {
     }
 
 
-    // Validation cellier
+/*---------Supprimer un cellier------------------*/ 
+
+
+
+    // Validation cellier 
     function validationCellier(nom_cellier, type_cellier_id){
         if(nom_cellier.value !== ""){     
             erreurnom.textContent = '';
@@ -325,7 +336,7 @@ window.addEventListener('load', function () {
     }
 
 
-    // Requête fetch cellier
+    // Requête fetch cellier 
     function fetchCellier(requete){
         fetch(requete)
             .then(response => {
@@ -354,6 +365,8 @@ window.addEventListener('load', function () {
                 console.error(error);
             });
     }
+/********************************************************************************************************** */
+    
 
 
     
--- js/modal.js
+++ js/modal.js
@@ -2,31 +2,36 @@
 
 /* MODAL */
 
-/*
-*   GESTION DU CELLIER
-*/
+let elModal = document.querySelector('[data-js-modal]'),
+
+// BOUTON ET TITRE
+elBoutonModal = document.querySelector('[data-js-boutonmodal]'),
+elTitreModal = document.querySelector('[data-js-titremodal]'),
+
+
+
+/********************************************************************************************************** */
+/*                          GESTION DU CELLIER      *GM                                                     */
+/********************************************************************************************************** */
 // AJOUTER CELLIER
-let elNouveauCellier = document.querySelector('[data-js-nouveaucellier]'),
+    elNouveauCellier = document.querySelector('[data-js-nouveaucellier]'),
 
 // MODIFIER CELLIER
     elModifierCellier = document.querySelectorAll('[data-js-modifiercellier]'),
 
+// SUPPRIMER CELLIER
+    elSupprimerCellier = document.querySelectorAll('[data-js-supprimercellier]'),
+
 // ANNULER
     elAnnulercellier = document.querySelector('[data-js-annulercellier]'),
+    elContenu = document.querySelector('[data-js-modalcontenu]')
 
-// MODAL
-    elModal = document.querySelector('[data-js-modal]'),
-
-// BOUTON ET TITRE
-    elBoutonModal = document.querySelector('[data-js-boutonmodal]'),
-    elTitreModal = document.querySelector('[data-js-titremodal]')
-
-
-
+  
 // AJOUTER UN CELLIER
     //ouvre
 if(elNouveauCellier){
-    elNouveauCellier.addEventListener('click', ()=>{
+    elNouveauCellier.addEventListener('click', (e)=>{
+        e.preventDefault();
         ouvre() 
         //Ajout titre
         elTitreModal.innerHTML = "Ajouter un cellier"
@@ -37,17 +42,21 @@ if(elNouveauCellier){
 }
 //FERMER MODAL
 if(elAnnulercellier){
-    elAnnulercellier.addEventListener('click', () => {
+    elAnnulercellier.addEventListener('click', (e) => {
+        
+        e.preventDefault();
         ferme()
         elBoutonModal.firstElementChild.remove()
     });
+    
 }
 
 // MODIFIER CELLIER
     //ouvre
     elModifierCellier.forEach(btnModifier=>{
         if(btnModifier){
-            btnModifier.addEventListener('click', ()=>{
+            btnModifier.addEventListener('click', (e)=>{
+                e.preventDefault();
                 ouvre() 
                 //Récupérer le nom du cellier
                 let nomCellier = btnModifier.parentNode.dataset.jsNomcellier
@@ -58,10 +67,49 @@ if(elAnnulercellier){
             })
         }
     })
-    
- /*
- *  FIN DU CELLIER
- */  
+
+// SUPPRIMER CELLIER
+    // ouvre    
+    elSupprimerCellier.forEach(btnSupprimer=>{
+        if(btnSupprimer){
+            btnSupprimer.addEventListener('click', (e)=>{
+                e.preventDefault();
+                ouvre()
+      
+                //Récupérer le nom du cellier
+                let nomCellier = btnSupprimer.parentNode.dataset.jsNomcellier
+       
+        
+                elContenu.innerHTML = `<h4 class=""><span class="carte__erreur">Supprimer le cellier</span> "${nomCellier}" ?</h4>
+                <p class="modal__texte">Cette action entraînera la suppression du cellier et de toutes ces bouteilles</p>
+                <div>
+                    <h4 class="carte__entete carte--haut">Déplacer les bouteilles dans un autre cellier?</h4>
+                    <label class="modal__texte" for="celliers">Choisir un cellier
+                        <select >
+                            <option></option>
+                        </select>
+                    </label>
+                </div>
+                
+                <div class="formulaire__champs" data-js-boutonmodal>
+                    <button  class="bouton-secondaire" data-js-supprimerInfosCellier>Déplacer et supprimer</button>
+                    <button  class="bouton-secondaire" data-js-supprimerInfosCellier>Supprimer</button>
+                    <button data-js-annulercellier class="bouton-secondaire">Annuler</button>
+                </div> 
+                `
+
+                // Fermer le modal
+                let annulerSupression = document.querySelector('[data-js-annulercellier]')
+                annulerSupression.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    ferme()  
+                });
+            })
+        }
+    })
+
+/********************************************************************************************************** */
+
 
 
 // Comportement du modal
